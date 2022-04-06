f8b34820b59be8b09c37d5b047b45f359e2981ab
AIphaKuma <74193783+AIphaKuma@users.noreply.github.com>
2022-04-06 20:27:42 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import RenderForm from './Component/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <RenderForm />
+    </div>
+  );
+}
+
+export default App;
